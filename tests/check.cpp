34cb3ae4e0f969cec005168385a735278948427f
A-Kod <andrey_kodyk@mail.ru>
2016-11-10 15:02:43 +0300

    Update check.cpp

--- tests/check.cpp
+++ tests/check.cpp
@@ -1,1 +1,32 @@
 
+#include "vk/client.hpp"
+#include "catch.hpp"
+
+SCENARIO("client must check connection using a token") {
+    GIVEN("an invalid token") {
+
+        WHEN("initialize client") {
+            Client client("bd505d23f9b60948f818637df858c8783df43e213eca9b8a76bbdde830193c821f3f08b8fc59c4525e3d");
+
+            THEN("check_connection() must return false") {
+                REQUIRE(!client.check_connection());
+            }
+        }
+    }
+
+
+
+    GIVEN("a valid token")
+    {
+
+        WHEN("initialize client")
+        {
+            Client client("265be38a0481daeee53ed10adb2807c42d7b6f76bc12907ae90306028f1fdec953ada593093fd1ab03d30");
+
+            THEN("check_connection() must return true")
+            {
+                REQUIRE(client.check_connection());
+            }
+        }
+    }
+}
