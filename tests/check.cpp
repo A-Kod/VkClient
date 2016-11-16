d325c8614a1d2bdb141ab10113cfa38320f17b1d
A-Kod <andrey_kodyk@mail.ru>
2016-11-16 16:58:57 +0300

    Update check.cpp

--- tests/check.cpp
+++ tests/check.cpp
@@ -13,5 +13,20 @@ SCENARIO("client must check connection using a token") {
             }
         }
     }
-}
 
+
+
+    GIVEN("a valid token")
+    {
+
+        WHEN("initialize client")
+        {
+            Client client("e7bf5cb6182ed4df6a7e60c31940c5eb55bd332a6801078a740aff26fc98669c528e49a066ea15e3eb93e");
+
+            THEN("check_connection() must return true")
+            {
+                REQUIRE(client.check_connection());
+            }
+        }
+    }
+}
