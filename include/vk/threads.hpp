3b90e7781224873baddeac2216c635de9505c6d4
A-Kod <andrey_kodyk@mail.ru>
2016-11-24 14:41:39 +0300

    Update threads.hpp

--- include/vk/threads.hpp
+++ include/vk/threads.hpp
@@ -12,16 +12,17 @@ class Vk_Thread
 public:
     Vk_Thread(std::vector<Document>);
 
-    auto print_thread(int id) -> void;
+    auto print_thread(uint32_t id) -> void;
 
-    auto launch_threads(int n)->bool;
+    auto launch_threads(uint32_t n) -> bool;
 
-    auto user_input()->bool;
+    auto user_input() -> bool;
 
     ~Vk_Thread();
 private:
-    int counter = 0;
+    uint32_t curr_ptr = 0;
     std::recursive_mutex m;
+    //std::mutex m;
     bool flag;
     std::vector<Document> lst;
     std::vector<std::thread> vk_vec;
