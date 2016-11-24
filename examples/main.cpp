a71d4f748a796b9e25f9b5710a575c19c40a4fff
A-Kod <andrey_kodyk@mail.ru>
2016-11-24 11:29:34 +0300

    Update main.cpp

--- examples/main.cpp
+++ examples/main.cpp
@@ -2,8 +2,8 @@
 #include <thread>
 #include <vector>
 #include <future>
-#include "vk/client.hpp"
-#include "vk/threads.hpp"
+#include "../client.hpp"
+#include "../threads.hpp"
 //#include "sources/client.cpp"
 
 std::vector<Document> lst;
