020a0736c3c06bb2919af97299125601f9424e94
A-Kod <andrey_kodyk@mail.ru>
2016-11-24 10:54:33 +0300

    Create threads.hpp

---
+++ include/vk/threads.hpp
@@ -0,0 +1,31 @@
+#ifndef VKCLIENT_THREADS_H
+#define VKCLIENT_THREADS_H
+
+#include <thread>
+#include <future>
+#include <chrono>
+#include <vector>
+#include <ctime>
+#include "client.hpp"
+class Vk_Thread
+{
+public:
+    Vk_Thread(std::vector<Document>);
+
+    auto print_thread(int id) -> void;
+
+    auto launch_threads(int n)->bool;
+
+    auto cin()->bool;
+
+    ~Vk_Thread();
+private:
+    int counter = 0;
+    std::recursive_mutex m;
+    bool flag;
+    std::vector<Document> lst;
+    std::vector<std::thread> vk_vec;
+
+};
+#endif //VKCLIENT_THREADS_H
+
