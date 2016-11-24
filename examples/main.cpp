f1e684abdfaea171ff1f4738f40992157568cb14
A-Kod <andrey_kodyk@mail.ru>
2016-11-24 14:41:04 +0300

    Update main.cpp

--- examples/main.cpp
+++ examples/main.cpp
@@ -2,36 +2,35 @@
 #include <thread>
 #include <vector>
 #include <future>
-#include <vk/client.hpp>
-#include <vk/threads.hpp>
-#include "../client.cpp"
-#include "../threads.cpp"
+#include "vk/client.hpp"
+#include "vk/threads.hpp"
 
 std::vector<Document> lst;
 
 int main()
 {
-    std::cout<<std::endl;
+    std::cout<<std::endl<<"hardware_concurrency: "<<std::endl;
     std::cout<<std::thread::hardware_concurrency()<<std::endl;
     std::cout<<std::endl;
-    Client client (""); //старый "просроченный" токен
+    Client client ("7d10cec10d0b87df392c5aa0d1b13456591c6d0b1fa3b0a9ef2ed232360efc52b131b620b96d37a3c2c96&"); //старый "просроченный" токен
     if (client.check_connection())
     {
-        std::cout<<std::endl<<"Удалось подключиться"<<std::endl;
+        std::cout<<std::endl<<"Successfully connected!"<<std::endl;
         lst = client.get_docs();
         Vk_Thread th(lst);
         if (th.user_input())
         {
-            std::cout << "all_threads_end" << std::endl;
+            std::cout << "END" << std::endl;
         }
         else
         {
-            std::cout << "error";
+            std::cout << "EGGOG";
         }
 
     }
     else
-        std::cout<<std::endl<<"Не удалось подключиться"<<std::endl;
+        std::cout<<std::endl<<"Connection failed!"<<std::endl;
     return 0;
 }
+
 
