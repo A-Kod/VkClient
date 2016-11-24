8203f91441faa81b51af692647f911b8dbd2cfcc
A-Kod <andrey_kodyk@mail.ru>
2016-11-24 12:02:53 +0300

    Update main.cpp

--- examples/main.cpp
+++ examples/main.cpp
@@ -14,7 +14,7 @@ int main()
     std::cout<<std::endl;
     std::cout<<std::thread::hardware_concurrency()<<std::endl;
     std::cout<<std::endl;
-    Client client ("7d10cec10d0b87df392c5aa0d1b13456591c6d0b1fa3b0a9ef2ed232360efc52b131b620b96d37a3c2c96&"); //старый "просроченный" токен
+    Client client (""); //старый "просроченный" токен
     if (client.check_connection())
     {
         std::cout<<std::endl<<"Удалось подключиться"<<std::endl;
